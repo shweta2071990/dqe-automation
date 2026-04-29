03713a393069b5cd4eda52fa5166c09299ba2df3
Shweta Bansal <shweta_bansal@epam.com>
2026-04-29 19:46:48 +0530

    added ParquetReader class

---
+++ PyTest DQ Framework/src/connectors/file_system/parquet_reader.py
@@ -0,0 +1,8 @@
+class ParquetReader:
+    def __init__(self, file_path=None):
+        self.file_path = file_path
+
+    def read(self):
+        # placeholder logic
+        print("Reading parquet file...")
+        return None
